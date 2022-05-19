637a5eb239843843028ae1137c9ab50172ff0d7b
shiba inu <jefferson121996@gmail.com>
2022-05-19 16:44:06 -0400

    Returning notes.html and index.html with no error

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require ('express');
 const app = express();
+const {notes} = require('./Develop/db/db.json');
+const path = require('path');
+const res = require('express/lib/response');
 
 
 
 
 
+app.get('/api/notes', (req, res) => {
+    res.send(notes);
+})
+
+app.get('/notes', (req, res)=> {
+    res.sendFile(path.join(__dirname,'./Develop/public/notes.html'));
 
 
+});
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'))
 
-
+}); 
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001`);
 });
+
+
